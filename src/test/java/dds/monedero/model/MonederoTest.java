e77cea5090814d80285e2a4d1f12245bca5e720f
Manuel Garcia F. <mgarciafrigo@gmail.com>
2024-04-22 15:27:18 -0300

    arreglar tests

--- src/test/java/dds/monedero/model/MonederoTest.java
+++ src/test/java/dds/monedero/model/MonederoTest.java
@@ -1,13 +1,13 @@
 package dds.monedero.model;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import dds.monedero.exceptions.MaximaCantidadDepositosException;
 import dds.monedero.exceptions.MaximoExtraccionDiarioException;
 import dds.monedero.exceptions.MontoNegativoException;
 import dds.monedero.exceptions.SaldoMenorException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MonederoTest {
   private Cuenta cuenta;
@@ -20,6 +20,7 @@ public class MonederoTest {
   @Test
   void Poner() {
     cuenta.poner(1500);
+    assertEquals(1500, cuenta.getSaldo());
   }
 
   @Test
@@ -29,26 +30,30 @@ public class MonederoTest {
 
   @Test
   void TresDepositos() {
-    cuenta.poner(1500);
-    cuenta.poner(456);
-    cuenta.poner(1900);
+    // Hasta tres depositos no deberia lanzar excepcion
+    assertDoesNotThrow(() -> {
+      cuenta.poner(1500);
+      cuenta.poner(456);
+      cuenta.poner(1900);
+    });
+    assertEquals(1500 + 456 + 1900, cuenta.getSaldo());
   }
 
   @Test
   void MasDeTresDepositos() {
     assertThrows(MaximaCantidadDepositosException.class, () -> {
-          cuenta.poner(1500);
-          cuenta.poner(456);
-          cuenta.poner(1900);
-          cuenta.poner(245);
+      cuenta.poner(1500);
+      cuenta.poner(456);
+      cuenta.poner(1900);
+      cuenta.poner(245);
     });
   }
 
   @Test
   void ExtraerMasQueElSaldo() {
     assertThrows(SaldoMenorException.class, () -> {
-          cuenta.setSaldo(90);
-          cuenta.sacar(1001);
+      cuenta.setSaldo(90);
+      cuenta.sacar(91);
     });
   }
 
